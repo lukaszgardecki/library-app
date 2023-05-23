faa872cd65027da19229ad4c76f452b336b88eb0
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-05-23 19:39:03 +0200

    Change request app path to /api/v1

--- src/main/java/com/example/libraryapp/web/BookController.java
+++ src/main/java/com/example/libraryapp/web/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class BookController {
     private final BookService bookService;
 
