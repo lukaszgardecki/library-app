c8bde099fd9f46fea1164b76b87eeb1b1048ee52
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-05-25 23:54:41 +0200

    Add 'produces' parameter in the BookController

--- src/main/java/com/example/libraryapp/web/BookController.java
+++ src/main/java/com/example/libraryapp/web/BookController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(value = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
     private final BookService bookService;
 
