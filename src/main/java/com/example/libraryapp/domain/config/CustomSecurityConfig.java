0aab4e9d51e03a65357fe3781d4553be2777d741
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-06-01 00:08:35 +0200

    Update security filter chain

--- src/main/java/com/example/libraryapp/domain/config/CustomSecurityConfig.java
+++ src/main/java/com/example/libraryapp/domain/config/CustomSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.libraryapp.domain.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,13 +19,15 @@ public class CustomSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET,"/api/v1/admin/**").hasRole(ADMIN_ROLE)
-                        .requestMatchers(HttpMethod.POST, "/api/v1/register").permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN_ROLE)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/**").hasRole(ADMIN_ROLE)
+                        .requestMatchers(
+                                "/api/v1/users/**",
+                                "/api/v1/checkouts/**",
+                                "/api/v1/reservations/**"
+                        ).authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/books").authenticated()
                         .anyRequest().permitAll())
-                .formLogin(login -> login
-                        .loginPage("/login").permitAll())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login?logout").permitAll())
                 .rememberMe(remember -> remember
                         .key("aBcdeFgHijklMNopqRstUvwxYZ1234567890")
                         .tokenValiditySeconds(60 * 60 * 24 * 30)
