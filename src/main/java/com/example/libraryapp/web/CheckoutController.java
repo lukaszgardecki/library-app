d1ddd531ec8e7057450d0b9943d33e0dd89331ed
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-06-06 23:13:40 +0200

    Update GET /checkouts/{id} endpoint

--- src/main/java/com/example/libraryapp/web/CheckoutController.java
+++ src/main/java/com/example/libraryapp/web/CheckoutController.java
@@ -3,10 +3,12 @@ package com.example.libraryapp.web;
 import com.example.libraryapp.domain.checkout.CheckoutDto;
 import com.example.libraryapp.domain.checkout.CheckoutService;
 import com.example.libraryapp.domain.checkout.CheckoutToSaveDto;
+import com.example.libraryapp.domain.config.CustomSecurityConfig;
 import com.example.libraryapp.domain.config.assembler.CheckoutModelAssembler;
 import com.example.libraryapp.domain.exception.*;
 import com.example.libraryapp.domain.reservation.ReservationDto;
 import com.example.libraryapp.domain.reservation.ReservationService;
+import com.example.libraryapp.domain.user.UserService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
@@ -25,13 +27,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class CheckoutController {
     private final CheckoutService checkoutService;
     private final ReservationService reservationService;
+    private final UserService userService;
     private final CheckoutModelAssembler checkoutModelAssembler;
 
     public CheckoutController(CheckoutService checkoutService,
                               ReservationService reservationService,
+                              UserService userService,
                               CheckoutModelAssembler checkoutModelAssembler) {
         this.checkoutService = checkoutService;
         this.reservationService = reservationService;
+        this.userService = userService;
         this.checkoutModelAssembler = checkoutModelAssembler;
     }
 
@@ -60,10 +65,20 @@ public class CheckoutController {
 
     @GetMapping("/checkouts/{id}")
     public ResponseEntity<EntityModel<CheckoutDto>> getCheckoutById(@PathVariable Long id) {
-        return checkoutService.findCheckoutById(id)
-                .map(checkoutModelAssembler::toModel)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        try {
+            EntityModel<CheckoutDto> checkoutDtoEntityModel = checkoutService.findCheckoutById(id)
+                    .map(checkoutModelAssembler::toModel)
+                    .orElseThrow(CheckoutNotFoundException::new);
+            boolean requestFromOwner = userService.getCurrentLoggedInUserId() == checkoutDtoEntityModel.getContent().getUserId();
+            boolean requestFromAdmin = userService.getCurrentLoggedInUserRole().equals(CustomSecurityConfig.ADMIN_ROLE);
+
+            if (requestFromOwner || requestFromAdmin) {
+                return ResponseEntity.ok(checkoutDtoEntityModel);
+            }
+            throw new CheckoutNotFoundException();
+        } catch (CheckoutNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/checkouts")
