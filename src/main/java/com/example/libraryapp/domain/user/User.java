09e1a8a687cd90efd95d112d73d32df38a3b82ac
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-06-06 22:51:53 +0200

    Add checkouts list and reservations list to user

--- src/main/java/com/example/libraryapp/domain/user/User.java
+++ src/main/java/com/example/libraryapp/domain/user/User.java
@@ -1,8 +1,12 @@
 package com.example.libraryapp.domain.user;
 
+import com.example.libraryapp.domain.checkout.Checkout;
+import com.example.libraryapp.domain.reservation.Reservation;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -20,4 +24,8 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private UserRole role;
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<Checkout> checkouts;
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<Reservation> reservations;
 }
