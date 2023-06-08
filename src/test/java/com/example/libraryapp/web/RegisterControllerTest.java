8e10c78c2a18cd5ba42c70dfb9aa68a9c4c07ce9
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-06-08 16:57:24 +0200

    Update RegisterControllerTest

--- src/test/java/com/example/libraryapp/web/RegisterControllerTest.java
+++ src/test/java/com/example/libraryapp/web/RegisterControllerTest.java
@@ -7,17 +7,20 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class RegisterControllerTest {
 
     @Autowired
@@ -25,6 +28,7 @@ public class RegisterControllerTest {
 
     @Test
     @DirtiesContext
+    @Transactional
     void shouldCreateANewUserIfEmailIsUnique() {
         UserRegistrationDto userToSave = getUserRegistrationDtoWithUniqueEmail();
         ResponseEntity<String> createResponse = restTemplate
