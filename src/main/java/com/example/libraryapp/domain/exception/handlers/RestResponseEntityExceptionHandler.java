6d912149981077b2f730f7927190fee91626e2f6
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-09-02 21:02:48 +0200

    Fix exceptions in BookController
    The try-catch clauses have been replaced by controller
    advice (exception handler).

---
+++ src/main/java/com/example/libraryapp/domain/exception/handlers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.libraryapp.domain.exception.handlers;
+
+import com.example.libraryapp.domain.exception.BookIsNotAvailableException;
+import com.example.libraryapp.domain.exception.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> mismatchExceptionHandler() {
+        return ResponseEntity.badRequest().body("Id must be a number.");
+    }
+
+    @ExceptionHandler(value = {BookNotFoundException.class, NullPointerException.class})
+    public ResponseEntity<String> bookNotFoundExceptionHandler() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(BookIsNotAvailableException.class)
+    public ResponseEntity<String> bookIsNotAvailableExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                .header("Reason", e.getMessage())
+                .build();
+    }
+}
