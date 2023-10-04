e6ccc922fe7cd386b3a838ccf25fe79268c9e680
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-10-04 22:17:24 +0200

    Add lombok to some classes and remove unnecessary ones

--- src/main/java/com/example/libraryapp/domain/card/LibraryCard.java
+++ src/main/java/com/example/libraryapp/domain/card/LibraryCard.java
@@ -2,14 +2,16 @@ package com.example.libraryapp.domain.card;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "library_card")
 public class LibraryCard {
     @Id
