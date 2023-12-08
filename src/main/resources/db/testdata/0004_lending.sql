7a7d374a350e16b49b37fd13d1a23658d83b74d8
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-12-08 21:49:42 +0100

    Update liquibase files and test data files

---
+++ src/main/resources/db/testdata/0004_lending.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset lukas:0003
+insert into
+    lending(creation_date, due_date, return_date, status, member_id, book_item_id)
+values
+    ('2023-05-21', '2023-06-20', '2023-06-10', 'COMPLETED', 1, 1),
+    ('2023-05-22', '2023-06-21', '2023-06-11', 'COMPLETED', 2, 2),
+    ('2023-05-23', '2023-06-22', null, 'CURRENT', 1, 3),
+    ('2023-05-24', '2023-06-23', null, 'CURRENT', 4, 4),
+    ('2023-05-25', '2023-06-24', null, 'CURRENT',1, 19),
+    ('2023-05-26', '2023-06-25', '2023-06-12', 'COMPLETED', 6, 6);
