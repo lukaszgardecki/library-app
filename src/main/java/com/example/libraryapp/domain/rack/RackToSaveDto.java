d5925d073497cc13fae68140311c29657a165429
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-02-15 20:43:28 +0100

    Add rack dtos + mapper

---
+++ src/main/java/com/example/libraryapp/domain/rack/RackToSaveDto.java
@@ -0,0 +1,12 @@
+package com.example.libraryapp.domain.rack;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RackToSaveDto {
+    private String locationIdentifier;
+}
