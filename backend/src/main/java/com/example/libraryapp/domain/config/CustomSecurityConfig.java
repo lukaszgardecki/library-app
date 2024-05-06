5c8c6871900146ab49abee8d71e9b5a92a5a9637
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-05-06 22:59:55 +0200

    Add cors configuration for angular

--- backend/src/main/java/com/example/libraryapp/domain/config/CustomSecurityConfig.java
+++ backend/src/main/java/com/example/libraryapp/domain/config/CustomSecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,9 +41,21 @@ public class CustomSecurityConfig {
                                 (request, response, authentication) -> SecurityContextHolder.clearContext()
                         ))
                 .csrf().disable()
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
         ;
         http.headers().frameOptions().sameOrigin();
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
