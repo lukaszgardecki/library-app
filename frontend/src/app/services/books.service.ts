eb3ea41f508ba5ccc92aebab02cef174d1768c8f
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-05-08 20:34:11 +0200

    Create service to fetch books from db
    Creating a simple service with one method
    to fetch all books (with paging).

---
+++ frontend/src/app/services/books.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BookItemsPage } from '../models/book-items-page';
+import { BooksPage } from '../models/books-page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  private baseURL = "http://localhost:8080/api/v1/books"
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(page: number, size: number, sort: string): Observable<BooksPage> {
+    const params = new HttpParams().set("page", page).set("size", size).set("sort", sort);
+    return this.http.get<BooksPage>(this.baseURL, {params: params});
+  }
+}
