5c78c46988fd2664f442f7d53fffe288393bc744
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-05-28 23:12:02 +0200

    Change @PreAuthorize to custom @RoleAuthorization
    Changed default security @PreAuthorize annotation to custom one.

--- backend/src/main/java/com/example/libraryapp/web/BookController.java
+++ backend/src/main/java/com/example/libraryapp/web/BookController.java
@@ -4,6 +4,7 @@ import com.example.libraryapp.domain.book.BookService;
 import com.example.libraryapp.domain.book.dto.BookDto;
 import com.example.libraryapp.domain.book.dto.BookToSaveDto;
 import com.example.libraryapp.domain.bookItem.dto.BookItemDto;
+import com.example.libraryapp.domain.config.RoleAuthorization;
 import com.example.libraryapp.management.PairDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -11,12 +12,13 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static com.example.libraryapp.domain.member.Role.ADMIN;
 
 @RestController
 @RequestMapping(value = "/api/v1/books", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -53,10 +55,9 @@ public class BookController {
     }
 
     @PostMapping
-    @PreAuthorize("hasAuthority('admin:create')")
+    @RoleAuthorization({ADMIN})
     public ResponseEntity<BookDto> addBook(@RequestBody BookToSaveDto book) {
         BookDto savedBook = bookService.saveBook(book);
-
         URI savedBookUri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedBook.getId())
@@ -65,21 +66,21 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAuthority('admin:update')")
+    @RoleAuthorization({ADMIN})
     ResponseEntity<BookDto> replaceBook(@PathVariable Long id, @RequestBody BookDto book) {
         BookDto replacedBook = bookService.replaceBook(id, book);
         return ResponseEntity.ok(replacedBook);
     }
 
     @PatchMapping("/{id}")
-    @PreAuthorize("hasAuthority('admin:update')")
+    @RoleAuthorization({ADMIN})
     ResponseEntity<BookDto> updateBook(@PathVariable Long id, @RequestBody BookDto book) {
         BookDto updatedBook = bookService.updateBook(id, book);
         return ResponseEntity.ok(updatedBook);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAuthority('admin:delete')")
+    @RoleAuthorization({ADMIN})
     ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
