f9ae719992edbdcad28b8ba581f88581b50bb8e9
Łukasz Gardecki <105553857+lukaszgardecki@users.noreply.github.com>
2024-06-03 17:21:24 +0200

    [FEATURE][F-43] Implement borrowed items page (#56)
    * Create a service to fetch borrowed books from backend
    
    * Create the borrowed books page template
    
    * Create a PDF service
    Creating a PDF service that enables to save borrowed
    books' table to PDF file.
    Used libraries: html2canvas and jspdf.
    
    * Implement 'Save to file' button
    
    * Add links to borrowed items' titles

--- frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
+++ frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProfileSetting } from '../profile-dashboard.component';
+import { LendingService } from '../../services/lending.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Lending } from '../../models/lending';
+import { PdfService } from '../../services/pdf.service';
 
 @Component({
   selector: 'app-borrowed-items',
   templateUrl: './borrowed-items.component.html',
   styleUrl: './borrowed-items.component.css'
 })
-export class BorrowedItemsComponent implements ProfileSetting {
+export class BorrowedItemsComponent implements ProfileSetting, OnInit {
   name: string = "Borrowed items";
   routerLink: string = "borrowed-items";
+  lendings: Array<Lending>;
+  lendingService = inject(LendingService);
+  authService = inject(AuthenticationService);
+  pdfService = inject(PdfService);
+
+  ngOnInit(): void {
+    const userId = this.authService.currentUserId;
+    this.lendingService.getLendingsByUserId(userId).subscribe({
+      next: items => this.lendings = items._embedded.lendingDtoList
+    });
+    console.log(this.lendings);
+  }
+
+  public saveAsPDF(): void {
+    const data = document.getElementById('table');
+    this.pdfService.saveAsPDF(data, "borrowed-books");
+  }
 }
---
+++ frontend/src/app/services/lending.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigService } from './config.service';
+import { LendingsPage } from '../models/lendings-page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LendingService {
+  private baseURL;
+
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService
+  ) { 
+    let baseURL = configService.getApiUrl();
+    this.baseURL = `${baseURL}/lendings`;
+  }
+
+  getLendingsByUserId(id: number): Observable<LendingsPage> {
+    return this.http.get<LendingsPage>(`${this.baseURL}?memberId=${id}`, { withCredentials: true });
+  }
+}
