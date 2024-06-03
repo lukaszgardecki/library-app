d990e4734cfdc7c1e00b934624840187477baae5
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-03 15:00:13 +0200

    Create the borrowed books page template

--- frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
+++ frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProfileSetting } from '../profile-dashboard.component';
+import { LendingService } from '../../services/lending.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Lending } from '../../models/lending';
 
 @Component({
   selector: 'app-borrowed-items',
   templateUrl: './borrowed-items.component.html',
   styleUrl: './borrowed-items.component.css'
 })
-export class BorrowedItemsComponent implements ProfileSetting {
+export class BorrowedItemsComponent implements ProfileSetting, OnInit {
   name: string = "Borrowed items";
   routerLink: string = "borrowed-items";
+  borrowedItems: Array<Lending>;
+  lendingService = inject(LendingService);
+  authService = inject(AuthenticationService);
+
+  ngOnInit(): void {
+    const userId = this.authService.currentUserId;
+    this.lendingService.getLendingByUserId(userId).subscribe({
+      next: items => this.borrowedItems = items._embedded.lendingDtoList
+    });
+    console.log(this.borrowedItems);
+  }
 }
