1b0d82e6d2397b5f8fbcdd9d23b135ae45b72155
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-03 15:19:00 +0200

    Implement 'Save to file' button

--- frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
+++ frontend/src/app/profile-dashboard/borrowed-items/borrowed-items.component.ts
@@ -3,6 +3,7 @@ import { ProfileSetting } from '../profile-dashboard.component';
 import { LendingService } from '../../services/lending.service';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Lending } from '../../models/lending';
+import { PdfService } from '../../services/pdf.service';
 
 @Component({
   selector: 'app-borrowed-items',
@@ -15,6 +16,7 @@ export class BorrowedItemsComponent implements ProfileSetting, OnInit {
   borrowedItems: Array<Lending>;
   lendingService = inject(LendingService);
   authService = inject(AuthenticationService);
+  pdfService = inject(PdfService);
 
   ngOnInit(): void {
     const userId = this.authService.currentUserId;
@@ -22,5 +24,10 @@ export class BorrowedItemsComponent implements ProfileSetting, OnInit {
       next: items => this.borrowedItems = items._embedded.lendingDtoList
     });
     console.log(this.borrowedItems);
+  }
+
+  public saveAsPDF(): void {
+    const data = document.getElementById('table');
+    this.pdfService.saveAsPDF(data, "borrowed-books");
   }
 }
