8a11083f4c272122a641baf4ea45e14e893a641a
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-04 00:18:58 +0200

    Create the completed requests page template

--- frontend/src/app/profile-dashboard/requested-items/requested-items-completed/requested-items-completed.component.ts
+++ frontend/src/app/profile-dashboard/requested-items/requested-items-completed/requested-items-completed.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProfileSetting } from '../../profile-dashboard.component';
+import { Reservation } from '../../../models/reservation';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { PdfService } from '../../../services/pdf.service';
+import { ReservationService } from '../../../services/reservation.service';
 
 @Component({
   selector: 'app-requested-items-completed',
   templateUrl: './requested-items-completed.component.html',
   styleUrl: './requested-items-completed.component.css'
 })
-export class RequestedItemsCompletedComponent implements ProfileSetting {
+export class RequestedItemsCompletedComponent implements ProfileSetting, OnInit {
   name: string = "Completed";
   routerLink: string = "completed";
+  reservationService = inject(ReservationService);
+  completedReservations: Array<Reservation>;
+  authService = inject(AuthenticationService);
+  pdfService = inject(PdfService);
+
+  ngOnInit(): void {
+    const userId = this.authService.currentUserId;
+    this.reservationService.getPendingLendingsByUserId(userId).subscribe({
+      next: reservationPage => {
+        if (reservationPage._embedded) {
+          this.completedReservations = reservationPage._embedded.reservationResponseList.filter(res => res.status === "READY");
+        }
+      }
+    });
+  }
+
+  public saveAsPDF(): void {
+    const data = document.getElementById('table');
+    this.pdfService.saveAsPDF(data, "requested-items-completed");
+  }
 }
