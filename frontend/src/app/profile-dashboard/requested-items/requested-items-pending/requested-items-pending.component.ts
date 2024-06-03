4c566b0f39f7e225b754bf0534d255235acb1a14
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-03 23:10:07 +0200

    Create the pending requests page template

--- frontend/src/app/profile-dashboard/requested-items/requested-items-pending/requested-items-pending.component.ts
+++ frontend/src/app/profile-dashboard/requested-items/requested-items-pending/requested-items-pending.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProfileSetting } from '../../profile-dashboard.component';
+import { ReservationService } from '../../../services/reservation.service';
+import { Reservation } from '../../../models/reservation';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { PdfService } from '../../../services/pdf.service';
 
 @Component({
   selector: 'app-requested-items-pending',
   templateUrl: './requested-items-pending.component.html',
   styleUrl: './requested-items-pending.component.css'
 })
-export class RequestedItemsPendingComponent implements ProfileSetting {
+export class RequestedItemsPendingComponent implements ProfileSetting, OnInit {
   name: string = "Pending";
   routerLink: string = "pending";
+  reservationService = inject(ReservationService);
+  pendingReservations: Array<Reservation>;
+  authService = inject(AuthenticationService);
+  pdfService = inject(PdfService);
+
+  ngOnInit(): void {
+    const userId = this.authService.currentUserId;
+    this.reservationService.getPendingLendingsByUserId(userId).subscribe({
+      next: reservationPage => {
+        if (reservationPage._embedded) {
+          this.pendingReservations = reservationPage._embedded.reservationResponseList.filter(res => res.status === "PENDING");
+        }
+      }
+    });
+  }
+
+  public saveAsPDF(): void {
+    const data = document.getElementById('table');
+    this.pdfService.saveAsPDF(data, "requested-items-pending");
+  }
 }
