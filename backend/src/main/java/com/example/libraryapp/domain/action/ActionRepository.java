bc3c0b6731c646b976656827a51016c2663c4267
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-05 22:49:56 +0200

    Create Action entity
    Created:
    - Entity class
    - Database table with liquibase
    - Repository

---
+++ backend/src/main/java/com/example/libraryapp/domain/action/ActionRepository.java
@@ -0,0 +1,7 @@
+package com.example.libraryapp.domain.action;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActionRepository extends JpaRepository<Action, Long> {
+
+}
