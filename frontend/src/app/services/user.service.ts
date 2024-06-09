52b6edb1343d2cefdad9c60c62583c4e15ff89e2
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-09 21:33:35 +0200

    Create service method to update password

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ConfigService } from './config.service';
 import { UserDetails } from '../models/user-details';
 import { Observable } from 'rxjs';
+import { UserUpdate } from '../shared/user-update';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,11 @@ export class UserService {
 
   getUserDetailsById(id: number): Observable<UserDetails> {
     return this.http.get<UserDetails>(`${this.baseURL}/${id}`, { withCredentials: true });
+  }
+
+  updatePassword(userId: number, password: string): Observable<UserDetails> {
+    const user = new UserUpdate();
+    user.password = password;
+    return this.http.patch<UserDetails>(`${this.baseURL}/${userId}`, user, { withCredentials: true });
   }
 }
