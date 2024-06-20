dcb10973846ba8c20439d470c1a8a12de0cb6e47
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-06-20 18:09:16 +0200

    Implement real-time notification list
    Implemented NotificationComponent with an HTML template and CSS,
    displaying a table of notifications updated in real-time. Added
    buttons for selecting all notifications at once and for deleting
    selected notifications.
    
    Notification titles are linked to future detail views for each
    notification, preparing for detailed notification display
    functionality.

--- frontend/src/app/profile-dashboard/notifications/notifications.component.ts
+++ frontend/src/app/profile-dashboard/notifications/notifications.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { ProfileSetting } from '../profile-dashboard.component';
+import { NotificationService } from '../../services/notification.service';
+import { Notification } from '../../models/notification';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
-export class NotificationsComponent implements ProfileSetting {
+export class NotificationsComponent implements ProfileSetting, OnInit {
   name: string = "Notifications";
   routerLink: string = "notifications";
+  notifications$: Observable<Notification[]>;
+  listIsEmpty: boolean;
+  allAreSelected: boolean = false;
+  @ViewChild("selectAll") selectAllBtn: ElementRef;
+  notificationService = inject(NotificationService);
+  router = inject(Router);
+
+  ngOnInit() {
+    this.notifications$ = this.notificationService.getNotifications();
+    this.notifications$.subscribe({
+      next: list => this.listIsEmpty = list.length === 0
+    })
+  }
+
+  showDetails(notification: Notification) {
+    this.notificationService.markNotificationAsRead(notification);
+    this.router.navigate(['/userprofile/notifications', notification.id]);
+  }
+
+  deleteSelected() {
+    this.notificationService.deleteAllSelected();
+  }
+
+  selectAll(event: Event) {
+    const button = (event.target as HTMLButtonElement);
+    this.notifications$.subscribe({
+      next: list => {
+        if (this.allAreSelected) {
+          list.forEach(el => el.selected = false);
+          this.allAreSelected = false;
+          button.textContent = "Select all"
+        } else {
+          list.forEach(el => el.selected = true);
+          this.allAreSelected = true;
+          button.textContent = "Unselect all"
+        }
+      }
+    });
+  }
 }
