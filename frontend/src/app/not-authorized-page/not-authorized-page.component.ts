67eb57cc616e30bd4de4e19007e55fc052907fc2
Łukasz Gardecki <lukas.g1804@gmail.com>
2024-07-09 19:02:41 +0200

    Frontend: Translate the error 403 page

--- frontend/src/app/not-authorized-page/not-authorized-page.component.ts
+++ frontend/src/app/not-authorized-page/not-authorized-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-not-authorized-page',
   templateUrl: './not-authorized-page.component.html',
-  styleUrl: './not-authorized-page.component.css'
+  styleUrl: './not-authorized-page.component.css',
 })
 export class NotAuthorizedPageComponent {
 
+  constructor(private translate: TranslateService) {
+    translate.use(localStorage.getItem(StorageService.SELECTED_LANG) || "en");
+  }
 }
