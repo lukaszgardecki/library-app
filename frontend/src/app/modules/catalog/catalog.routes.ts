5cd67d08bf32a468823edf6b659adc75ee7eb203
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-03-06 21:17:27 +0100

    Frontend: Update catalog routes

--- frontend/src/app/modules/catalog/catalog.routes.ts
+++ frontend/src/app/modules/catalog/catalog.routes.ts
@@ -10,12 +10,29 @@ export const catalogRoutes: Routes = [
       { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then((c) => c.DashboardComponent) },
       { path: 'notifications', loadComponent: () => import('./pages/notifications/notifications.component').then((c) => c.NotificationsComponent) },
       { path: 'messages', loadComponent: () => import('./pages/messages/messages.component').then((c) => c.MessagesComponent) },
-      { path: 'users', loadComponent: () => import('./pages/users/users.component').then((c) => c.UsersComponent) },
+      {
+        path: 'users', loadComponent: () => import('./pages/users/users.component').then((c) => c.UsersComponent),
+        data: { expectedRoles: ['ADMIN'] }
+      },
       { path: 'users/:id', loadComponent: () => import('./pages/user-details/user-details.component').then((c) => c.UserDetailsComponent) },
       { path: 'books', loadComponent: () => import('./pages/books/books.component').then((c) => c.BooksComponent) },
-      { path: 'cashier', loadComponent: () => import('./pages/cashier/cashier.component').then((c) => c.CashierComponent) },
-      { path: 'warehouse', loadComponent: () => import('./pages/warehouse/warehouse.component').then((c) => c.WarehouseComponent) },
-      { path: 'reports', loadComponent: () => import('./pages/reports/reports.component').then((c) => c.ReportsComponent) },
+      {
+        path: 'cashier', loadComponent: () => import('./pages/cashier/cashier.component').then((c) => c.CashierComponent),
+        data: { expectedRoles: ['ADMIN', 'CASHIER'] }
+      },
+      { 
+        path: 'warehouse',
+        data: { expectedRoles: ['ADMIN', 'WAREHOUSE'] },
+        children: [
+          { path: 'transactions', loadComponent: () => import('./pages/warehouse/transactions/transactions.component').then((c) => c.TransactionsComponent) },
+          { path: 'history', loadComponent: () => import('./pages/warehouse/history/history.component').then((c) => c.HistoryComponent) },
+          { path: 'racks', loadComponent: () => import('./pages/warehouse/racks/racks.component').then((c) => c.RacksComponent) }
+        ]
+      },
+      {
+        path: 'reports', loadComponent: () => import('./pages/reports/reports.component').then((c) => c.ReportsComponent),
+        data: { expectedRoles: ['ADMIN', 'WAREHOUSE', 'CASHIER'] }
+      },
       { path: 'settings', loadComponent: () => import('./pages/settings/settings.component').then((c) => c.SettingsComponent) },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full', },
     ],
